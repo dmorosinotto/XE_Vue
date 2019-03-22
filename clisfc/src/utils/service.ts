c5b7bcb680e8b764aea203ef559eb51f224eb476
Daniele Morosinotto <d.morosinotto@icloud.com>
2019-03-22 18:25:08 +0100

    (204c chiamate async/await)

--- clisfc/src/utils/service.ts
+++ clisfc/src/utils/service.ts
@@ -37,3 +37,17 @@ function _safeGet<T>(url: string, def: T): Promise<T> {
 }
 
 let _cache: IEvent[] = [];
+
+// 204c export function getAll / getById with handle logic for "_cache-first" win!
+export async function getAll(force: boolean = false): Promise<IEvent[]> {
+	if (force || !_cache || !_cache.length) {
+		const url = `${_baseurl}/GetUpcaming`;
+		return (_cache = await _safeGet(url, [] as IEvent[]));
+	} else return Promise.resolve(_cache);
+}
+
+export async function getById(eid: number | string): Promise<EventOrNull> {
+	const found = _cache && _cache.find(e => e.EventId == eid);
+	if (found) return Promise.resolve(found);
+	else return _safeGet(_baseurl + "/Get/" + eid, null);
+}
