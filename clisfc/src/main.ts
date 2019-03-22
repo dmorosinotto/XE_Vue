a408de75760abadd1e32aef5b7afa4fe68e119ec
Daniele Morosinotto <d.morosinotto@icloud.com>
2019-03-22 18:16:05 +0100

    202 = Porto codice del globalFilter che è utilizzato nel componente (202b main.ts)

--- clisfc/src/main.ts
+++ clisfc/src/main.ts
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import registerGlobalFilter from "./utils/filter";
+// 202b import and run global filter registration
+registerGlobalFilter();
 
 Vue.config.productionTip = false;
 
