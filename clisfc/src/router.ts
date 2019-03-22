8e328c6bac91a3fe8a554906728e994f071d865c
Daniele Morosinotto <d.morosinotto@icloud.com>
2019-03-22 18:37:03 +0100

    208 = Modifico router.ts per aggiungere la "detail"

--- clisfc/src/router.ts
+++ clisfc/src/router.ts
@@ -18,6 +18,12 @@ export default new Router({
 			// this generates a separate chunk (about.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
 			component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
+		},
+		{
+			path: "/detail/:id", // 208 route with params
+			name: "detail",
+			// lazy load component (separete chunk detail.[hash].js thanks to WebPack + Vue CLI)
+			component: () => import(/* webpackChunkName: "detail" */ "./views/Detail.vue")
 		}
 	]
 });
