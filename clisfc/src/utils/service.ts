1e738cade5d927c3f0b0ffc5125e1c2280856ca4
Daniele Morosinotto <d.morosinotto@icloud.com>
2019-03-22 18:25:37 +0100

    204 = Aggiungo service.ts con logica tutta TS

--- clisfc/src/utils/service.ts
+++ clisfc/src/utils/service.ts
@@ -41,13 +41,13 @@ let _cache: IEvent[] = [];
 // 204c export function getAll / getById with handle logic for "_cache-first" win!
 export async function getAll(force: boolean = false): Promise<IEvent[]> {
 	if (force || !_cache || !_cache.length) {
-		const url = `${_baseurl}/GetUpcaming`;
-		return (_cache = await _safeGet(url, [] as IEvent[]));
-	} else return Promise.resolve(_cache);
+		const url = `${_baseurl}/GetUpcaming`; //ignore cache and reload data
+		return (_cache = await _safeGet(url, [] as IEvent[])); //store data in _cache before return it
+	} else return Promise.resolve(_cache); //else return data from cache without doing new API request
 }
 
 export async function getById(eid: number | string): Promise<EventOrNull> {
-	const found = _cache && _cache.find(e => e.EventId == eid);
-	if (found) return Promise.resolve(found);
-	else return _safeGet(_baseurl + "/Get/" + eid, null);
+	const found = _cache && _cache.find(e => e.EventId == eid); //TS infer IEvent | undefined!
+	if (found) return Promise.resolve(found /* if found in cache return value without new req*/);
+	else return _safeGet(_baseurl + "/Get/" + eid, null); //search online (single GET) + null if not found
 }
