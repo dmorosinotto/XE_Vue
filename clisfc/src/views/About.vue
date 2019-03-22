5dc15491a30bcd3b5b627504ed019c6a41f2b91f
Daniele Morosinotto <d.morosinotto@icloud.com>
2019-03-22 18:17:33 +0100

    (203a import component XeEvent)

--- clisfc/src/views/About.vue
+++ clisfc/src/views/About.vue
@@ -3,3 +3,17 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+// 203a import XeEvent component and register it
+import XeEvent from "../components/XeEvent.vue";
+
+@Component({
+  components: { XeEvent } //203a
+})
+export default class About extends Vue {
+  //
+}
+</script>
